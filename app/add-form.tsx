b7060030a41b888d266247c1a46a9c274028ae8a
Mie Haga <miehaga2@gmail.com>
2023-11-05 14:04:37 -0800

    add material ui

--- app/add-form.tsx
+++ app/add-form.tsx
@@ -2,16 +2,26 @@
 import { useFormState } from "react-dom";
 import { startProcessing } from "@/app/actions";
 import { useEffect, useState } from "react";
+import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import { Grid, ThemeProvider, Typography, createTheme } from "@mui/material";
 
 const initialState = {
     message: null,
 }
+const theme = createTheme({
+    typography: {
+        fontSize: 10,
+    },
+});
 
 export function AddForm() {
     const [showSuccess, setShowSuccess] = useState(false);
     const [, formAction] = useFormState(async (_: unknown, formData: FormData) => {
         console.log("Calling server action")
-        await startProcessing(formData)
+        // await startProcessing(formData)
+        console.log(formData)
         console.log("Server action compeleted successfully")
         setShowSuccess(true)
     }, initialState)
@@ -20,20 +30,29 @@ export function AddForm() {
 
     return (
         <>
-            <form className="flex flex-col w-fit" action={formAction}>
-                <input className="w-72"
-                    name="publickey"
-                    placeholder="Enter Public Key"
-                    required
-                />
-                <input className="w-36"
-                    name="prompt"
-                    placeholder="Enter Prompt"
-                    required
-                />
-                <button type="submit">Submit</button>
+            <Grid container justifyContent="center" sx={{ mt: 10 }}>
+                <Stack spacing={2} alignItems="center" justifyContent="center" sx={{ width: 1 / 2 }}>
+                    <ThemeProvider theme={theme}>
+                        <Typography variant="h4">Text To NFT</Typography>
+                        <Typography variant="h6">Enter a prompt and a public key of your wallet to generate an NFT.
+                            The NFT will be minted to the wallet associated with the public key.
+                            The prompt will be used to generate the NFT.
+                            *The NFT is minted on the testnet. </Typography>
+                    </ThemeProvider>
+                </Stack>
+            </Grid>
+            <form action={formAction}>
+                <Grid container justifyContent="center">
+                    <Stack spacing={2} alignItems="center" justifyContent="center" sx={{ mt: 10, width: 1 / 2 }}>
+                        <TextField id="outlined-basic" name="publickey" label="Enter Public Key" variant="outlined" sx={{ width: 1 }} required />
+                        <TextField id="outlined-basic" name="prompt" label="Enter Prompt" variant="outlined" sx={{ width: 1 }} required />
+                        <Button variant="outlined" type="submit">Submit</Button>
+                    </Stack>
+                </Grid>
             </form>
-            {showSuccess && <div>Success</div>}
+            <Grid container justifyContent="center">
+                {showSuccess && <div>Success</div>}
+            </Grid>
         </>
     )
 }
